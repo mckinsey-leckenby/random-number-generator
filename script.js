47d2b4c72c21f0f51caf651df8dab5f42ab0e61d
mckinseyleckenby <leckenbym11@gmail.com>
2022-02-07 19:24:01 -0600

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const number = document.querySelector(".number");
-const button = document.querySelector(".generate");
+const hex = document.querySelector('.hex')
+const button = document.querySelector('.generate')
 
-const generateNumber = () => {
-    //Generate number between 1 and 10
-    const randomNumber = Math.floor(Math.random() * 10 + 1);
-    number.innerHTML = randomNumber;
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8);
+    document.body.style.backgroundColor = "#" + randomColor;
+    hex.innerHTML = "#" + randomColor;
 }
 
-button.addEventListener("click", generateNumber)
+button.addEventListener("click", generateColor)
 
-generateNumber();
+generateColor();
 
+// let color = Math.random() ;
+// color = Math.random().toString(16);
+// color = Math.random().toString(16).substring(2, 8);
+
+// console.log(color)
